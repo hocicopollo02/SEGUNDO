329abb9d0f51e5aecc4b9dcf37fb72898a4a2ac9
Pablo <ubuntu@PORT-PABLO.localdomain>
2021-12-20 12:44:39 +0100

    REDES

--- REDES/pract7/ejercicio5.java
+++ REDES/pract7/ejercicio5.java
@@ -4,17 +4,23 @@ import java.net.*;
 
 public class ejercicio5  {
 
-	Date date = new Date();
-	String now = date.toString() + "\r\n";
+	static Date date = new Date();
+	static String now = date.toString() + "\r\n";
 
 	public static void main(String[] a) {
-		while(true) {
 			try{
 
-				ServerSocket ss = new ServerSocket();
+				DatagramSocket ds= new DatagramSocket(7777);
+				DatagramPacket p = new DatagramPacket(new byte[1024], 1024);
+				ds.receive(p);
+				p.setData(now.getBytes());
+				p.setLength(now.getBytes().length);
+				ds.send(p);
+				System.out.println(now);
+
 				
 			} catch(IOException e) { System.out.println(e); }
-		}
+		
 	}
 
 }
